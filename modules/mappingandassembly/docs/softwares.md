3972e6b6f0c62d94a06594216fb1b2343291a5e2
SarahL-UU <Sarah.Lideus.4744@student.uu.se>
2024-02-14 14:37:40 +0100

    Added some modules

---
+++ modules/mappingandassembly/docs/softwares.md
@@ -0,0 +1,1 @@
+# Softwares used in the mappingandassembly module
---
+++ modules/prealignmentqc/workflow/scripts/dummy.py
@@ -0,0 +1,11 @@
+# vim: syntax=python tabstop=4 expandtab
+# coding: utf-8
+
+__author__ = "Sarah Lideus"
+__copyright__ = "Copyright 2024, Sarah Lideus"
+__email__ = "Sarah.Lideus.4744@student.uu.se"
+__license__ = "GPL-3"
+
+
+def dummy():
+    return 1
